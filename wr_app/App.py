c94aaa6c4789709bbbbb70ef79c590f95b1f0715
scaracciolo <santiago.caracciolo@gmail.com>
2022-05-04 16:42:40 -0300

    refactor

--- wr_app/App.py
+++ wr_app/App.py
@@ -1,13 +1,13 @@
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QProgressBar, QProgressDialog, QSplitter, QVBoxLayout, QWidget
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import *
+from PySide6.QtCore import *
 import sys
 import numpy as np
 from wr_core import utils
 import vispy as vp
 from vispy import scene
-import os 
 from wr_app import Models, Panels, Constants
+
 class BeatViewer(QWidget):
     def __init__(self, parent=None) -> None:
         super().__init__(parent=parent)
@@ -20,13 +20,13 @@ class BeatViewer(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.canvas = vp.scene.SceneCanvas(show=True, bgcolor='white', parent=self)
+        self.canvas = scene.SceneCanvas(show=True, bgcolor='white', parent=self)
         self.grid = self.canvas.central_widget.add_grid()
 
         self.vb1 = self.grid.add_view(row=0, col=0, camera='panzoom')
         self.vb1.camera.interactive = False
 
-        self.line = vp.scene.Line(
+        self.line = scene.Line(
             pos = self._data,
             color = 'black',
             parent=self.vb1.scene
@@ -35,7 +35,7 @@ class BeatViewer(QWidget):
         # self.vb2 = self.grid.add_view(row=1, col=0, camera='panzoom')
         # self.vb2.camera.interactive = False
 
-        self.markers = vp.scene.Markers(
+        self.markers = scene.Markers(
             pos = self._markers,
             parent=self.vb1.scene
         )
@@ -44,17 +44,17 @@ class BeatViewer(QWidget):
         layout.addWidget(self.canvas.native)
         self.setLayout(layout)
 
-    def set_line(self, params):
+    def set_line(self, features):
         self.vb1.camera.set_range(
-            x=(0, 2*params[4]),
+            x=(0, 2*features[6]),
             y = Constants.Y_RANGE
         )
-        self._data[:,1] = utils.model(self._data[:,0], *params.tolist())
+        self._data[:,1] = utils.model(self._data[:,0], *features.tolist())
         self.line.set_data(pos=self._data)
 
-    def set_markers(self, fiducials, params):
+    def set_markers(self, fiducials, features):
         self._markers[:, 0] = fiducials
-        self._markers[:, 1] = utils.model(fiducials, *params.tolist())
+        self._markers[:, 1] = utils.model(fiducials, *features.tolist())
 
         self.markers.set_data(
             self._markers,
@@ -72,13 +72,13 @@ class EcgViewer(QWidget):
         self.setup_ui()
 
     def setup_ui(self):
-        self.canvas = vp.scene.SceneCanvas(show=True, bgcolor='white', parent=self)
+        self.canvas = scene.SceneCanvas(show=True, bgcolor='white', parent=self)
         self.grid = self.canvas.central_widget.add_grid()
 
         self.vb1 = self.grid.add_view(row=0, col=0, camera='panzoom')
         self.vb1.camera.interactive = True
 
-        self.line = vp.scene.Line(
+        self.line = scene.Line(
             color = 'black',
             parent=self.vb1.scene
         )
@@ -109,17 +109,17 @@ class ecgsyn(QMainWindow):
         super().__init__()
         self.app = app
 
-        self.params_model = Models.ParamsModel(Constants.params)
-        self.inputs_model = Models.InputsModel(Constants.inputs)
+        self.measurements_model = Models.MeasurementsModel(Constants.measurements)
+        self.features_model = Models.FeaturesModel(Constants.features)
         self.fiducials_model = Models.FiducialsModel(Constants.fiducials)
 
         self.setup_ui()
 
-        self.inputs_model.computeParams.connect(self.params_model.updateParams)
-        self.params_model.computeSignal.connect(self.update)
+        # self.measurements_model.computeFeatures.connect(self.features_model.updateFeatures)
+        self.features_model.computeSignal.connect(self.update)
         self.fiducials_model.drawMarkers.connect(self.beatViewer.set_markers)
 
-        self.inputs_model.setDefaultInputs(self.inputsPanel.default_median_values)
+        self.measurements_model.setDefaultMeasurements(self.measurementsPanel.default_median_values)
 
     def open_progress(self):
         self.progressDialog.setValue(50)
@@ -127,9 +127,9 @@ class ecgsyn(QMainWindow):
     def close_progress(self):
         self.progressDialog.reset()
 
-    def update(self, params):
-        self.beatViewer.set_line(params)
-        self.fiducials_model.updateFiducials(params)
+    def update(self, features):
+        self.beatViewer.set_line(features)
+        # self.fiducials_model.updateFiducials(features)
 
     def plot(self, ecg):
         self.noisePanel.add_noise(ecg[:,1])
@@ -138,11 +138,11 @@ class ecgsyn(QMainWindow):
     def setup_ui(self):
         self.setWindowTitle('ecgsyn')
 
-        self.inputsPanel = Panels.InputsPanel(parent=self)
+        self.measurementsPanel = Panels.MeasurementsPanel(parent=self)
         self.noisePanel = Panels.NoisePanel(parent=self)
         self.runPanel = Panels.RunPanel(parent=self)
-        # inputsView = TablePanel(self.inputs_model, parent=self)
-        # paramsTable = TablePanel(self.params_model, parent=self)
+        # measurementsView = TablePanel(self.measurements_model, parent=self)
+        # featuresTable = TablePanel(self.features_model, parent=self)
         # fiducialsTable = TablePanel(self.fiducials_model, parent=self)
         self.beatViewer = BeatViewer(parent=self)
         self.ecgViewer = EcgViewer(parent=self)
@@ -150,16 +150,16 @@ class ecgsyn(QMainWindow):
         # vertical splitter
         vsplitter = QSplitter(Qt.Vertical)
 
-        vsplitter.addWidget(self.inputsPanel)
+        vsplitter.addWidget(self.measurementsPanel)
         vsplitter.setStretchFactor(0, 3)
 
-        # vsplitter.addWidget(inputsView)
+        # vsplitter.addWidget(measurementsView)
         # vsplitter.setStretchFactor(1, 1)
 
         # vsplitter.addWidget(fiducialsTable)
         # vsplitter.setStretchFactor(2, 1)
 
-        # vsplitter.addWidget(paramsTable)
+        # vsplitter.addWidget(featuresTable)
         # vsplitter.setStretchFactor(3, 1)
 
         vsplitter.addWidget(self.noisePanel)
